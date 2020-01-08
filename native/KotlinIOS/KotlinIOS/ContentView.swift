bf4f7944a3959874d0106192818bab64bca04579
Nino Handler <nino.handler@arbeitsagentur.de>
2020-01-08 15:49:59 +0100

    Create empty iOS project

---
+++ native/KotlinIOS/KotlinIOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KotlinIOS
+//
+//  Created by Nino Handler on 08.01.20.
+//  Copyright © 2020 Nino Handler. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
