b1f0f97b01043fbdf2759b4a1b68fc2eff7fa93d
Nino Handler <nino.handler@arbeitsagentur.de>
2020-01-09 13:41:04 +0100

    Connect data in Android

--- app/src/main/java/de/handler/mpp/mobile/MainActivity.kt
+++ app/src/main/java/de/handler/mpp/mobile/MainActivity.kt
@@ -1,15 +1,24 @@
 package de.handler.mpp.mobile
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+    private val job = Job()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<TextView>(R.id.main_text_view).text = createApplicationScreenMessage()
+        launch {
+            findViewById<TextView>(R.id.main_text_view).text =
+                Repository.fetchPeople().joinToString(separator = "\n") { it.name }
+        }
+
     }
 }
